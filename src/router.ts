dd6cf1b558d4d0516e2d0a29efc005184df94f52
Nathan Dygant <nathan.dygant@gmail.com>
2025-06-24 11:04:00 +0200

    Switching to webhistory :)

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from 'vue-router';
+import { createWebHistory, createRouter } from 'vue-router';
 
 import WelcomePage from './pages/WelcomePage.vue';
 import WorkExperiencePage from './pages/WorkExperiencePage.vue';
@@ -23,7 +23,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes
 });
 
